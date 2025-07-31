32fe8041aee73de2b45392b67d1fb3fa63039103
nathan-renard <aimbire@proton.me>
2025-07-31 16:33:56 -0700

    CH2L3

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from python-ai-agent!")
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    if not sys.argv[1:]:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)],
+        )
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    # Check if verbose mode is enabled
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":    
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
+    print(response.text)
 
 if __name__ == "__main__":
     main()
